9679dca2d204fcf06f7ab03ce2443b7aab6c8a15
Nata <ganenkova@mail.ru>
2023-02-02 18:05:15 +0300

    Задача2

---
+++ Example001/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Ввведите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Ввведите число b: ");
+int b = int.Parse(Console.ReadLine());
+int max;
+if(a>b){
+    Console.WriteLine($"max = {a} min={b}");
+
+}else{
+     Console.WriteLine($"max = {b} min={a}");
+}
+Console.Clear();
+Console.Clear();
