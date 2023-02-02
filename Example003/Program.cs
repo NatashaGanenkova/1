0f3d270200a505c7f34bc0cc502d65632950df07
Nata <ganenkova@mail.ru>
2023-02-02 17:51:18 +0300

    :Задача6

---
+++ Example003/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число а:");
+int a = int.Parse(Console.ReadLine());
+if(a%2 == 0){
+    Console.WriteLine($" Число {a}четное");
+}else{
+    Console.WriteLine($" Число {a}не четное");
+}
