376e13de492b1de83987615b2dfbfedb22c0715c
Nata <ganenkova@mail.ru>
2023-02-02 18:29:41 +0300

    Задача 8

---
+++ Example004/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Введите число а:");
+int a = int.Parse(Console.ReadLine());
+int i = 1;
+while(i<=a){
+    if(i%2==0){
+    Console.Write($"{i},");
+    }
+    i++;
+}
+    
